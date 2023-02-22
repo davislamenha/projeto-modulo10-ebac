d575a1adfae0d8696d70af31b35cedb3fa96e298
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-22 18:34:21 -0300

    validacao implementada ao formulario

--- js/main.js
+++ js/main.js
@@ -10,3 +10,39 @@ $('.menu-hamburguer').on('click', function () {
 });
 
 $('#telefone').mask('(00) 00000-0000', { placeholder: '(00) 00000-0000' });
+
+$('form').validate({
+  rules: {
+    nome: {
+      required: true,
+    },
+    email: {
+      required: true,
+      email: true,
+    },
+    telefone: {
+      required: true,
+    },
+    veiculoInteresse: {
+      required: true,
+    },
+    mensagem: {
+      required: true,
+    },
+  },
+  messages: {
+    nome: 'Por favor, insira seu nome',
+    email: 'Este campo é obrigatório',
+    telefone: 'Este campo é obrigatório',
+    veiculoInteresse: 'Este campo é obrigatório',
+    mensagem: 'Este campo é obrigatório',
+  },
+  submitHandler: function (form) {
+    alert('Enviado com sucesso!');
+  },
+  invalidHandler: function (evento, validador) {
+    let camposIncorretos = validador.numberOfInvalids();
+    if (camposIncorretos)
+      alert(`Quantidade de campos incorretos: ${camposIncorretos}`);
+  },
+});
